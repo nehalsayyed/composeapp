b458f6a2dc4fd70a4af303234e36873746656f43
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2025-09-29 11:15:43 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation("androidx.navigation:navigation-compose:2.8.8")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
