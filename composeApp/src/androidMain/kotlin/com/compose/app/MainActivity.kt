90d4744833d54fd2b872e468366768b439e1c0fd
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2025-10-04 16:53:58 +0530

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/compose/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/compose/app/MainActivity.kt
@@ -26,105 +26,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-              val navController = rememberNavController()
-                Surface(color = Color.White) {
+              Surface(color = Color.White) {
                     // Scaffold Component
-                    Scaffold(
-                        // Bottom navigation
-                        bottomBar = {
-                            BottomNavigationBar(navController = navController)
-                        }, content = { padding ->
-                            // Nav host: where screens are placed
-                            NavHostContainer(navController = navController, padding = padding)
-                        }
-                    )
+                    Text("Nehal")
                 }
             }
         
     }
 }
 
-@Composable
-fun NavHostContainer(
-    navController: NavHostController,
-    padding: PaddingValues
-) {
-
-    NavHost(
-        navController = navController,
-
-        // set the start destination as home
-        startDestination = "home",
-
-        // Set the padding provided by scaffold
-        modifier = Modifier.padding(paddingValues = padding),
-
-        builder = {
-
-            // route : Home
-            composable("home") {
-                HomeScreen()
-            }
-
-            // route : search
-            composable("search") {
-                SearchScreen()
-            }
-
-            // route : profile
-            composable("profile") {
-                ProfileScreen()
-            }
-        })
-}
-
-@Composable
-fun BottomNavigationBar(navController: NavHostController) {
-
-    NavigationBar(
-
-        // set background color
-        containerColor = Color(0xFF0F9D58)) {
-
-        // observe the backstack
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-
-        // observe current route to change the icon
-        // color,label color when navigated
-        val currentRoute = navBackStackEntry?.destination?.route
-
-        // Bottom nav items we declared
-        Constants.BottomNavItems.forEach { navItem ->
-
-            // Place the bottom nav items
-            NavigationBarItem(
-
-                // it currentRoute is equal then its selected route
-                selected = currentRoute == navItem.route,
-
-                // navigate on click
-                onClick = {
-                    navController.navigate(navItem.route)
-                },
-
-                // Icon of navItem
-                icon = {
-                    Icon(imageVector = navItem.icon, contentDescription = navItem.label)
-                },
-
-                // label
-                label = {
-                    Text(text = navItem.label)
-                },
-                alwaysShowLabel = false,
-
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = Color.White, // Icon color when selected
-                    unselectedIconColor = Color.White, // Icon color when not selected
-                    selectedTextColor = Color.White, // Label color when selected
-                    indicatorColor = Color(0xFF195334) // Highlight color for selected item
-                )
-            )
-        }
-    }
-}
