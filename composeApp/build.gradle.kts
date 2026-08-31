4a838068a634057efd42a00516f7e54abd797e1b
nehalsayyed <73099770+nehalsayyed@users.noreply.github.com>
2026-08-31 13:52:34 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -30,7 +30,8 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation("androidx.compose.material:material-icons-extended:1.7.0")
+            implementation(compose.materialIconsExtended)
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
